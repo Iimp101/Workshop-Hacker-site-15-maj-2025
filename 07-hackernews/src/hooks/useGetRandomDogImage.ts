692d00201256b31c7217f640b078b2f1029d1a20
Magnus Lundberg <magnuslundberg86@live.com>
2025-05-20 14:30:06 +0200

    added two buttons for getting random dog and random shiba dog

--- 07-hackernews/src/hooks/useGetRandomDogImage.ts
+++ 07-hackernews/src/hooks/useGetRandomDogImage.ts
@@ -2,15 +2,15 @@ import { useEffect, useState } from 'react'
 import { RandomDogImage } from '../services/DogAPI.types';
 import axios from 'axios';
 
-const useGetRandomDogImage = () => {
+const useGetRandomDogImage = (url: string | null = null) => {
     const [data, setData] = useState<RandomDogImage | null>(null);
     const [isLoading, setIsLoading] = useState(false);
 	// 🦴
-	const getData = async () => {
+	const getData = async (resource: string) => {
         setData(null);
         setIsLoading(true);
 		// get data from api
-		const res = await axios.get<RandomDogImage>("https://dog.ceo/api/breeds/image/random");
+		const res = await axios.get<RandomDogImage>(resource);
 		await new Promise(r => setTimeout(r, 1500));
 
 		// update state with data
@@ -19,8 +19,9 @@ const useGetRandomDogImage = () => {
 	}
 
 	useEffect(() => {
-		getData();
-	}, []);
+        if (!url) return;
+		getData(url);
+	}, [url]);
 
     return {
         data,
--- 07-hackernews/src/pages/RandomDogPage.tsx
+++ 07-hackernews/src/pages/RandomDogPage.tsx
@@ -1,13 +1,24 @@
 import Image from "react-bootstrap/Image";
 import useGetRandomDogImage from "../hooks/useGetRandomDogImage";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
 
 const RandomDogPage = () => {
-	const { data, isLoading } = useGetRandomDogImage();
+    const [url, setUrl] = useState<string | null>("https://dog.ceo/api/breeds/image/random");
+	const { data, isLoading } = useGetRandomDogImage(url);
 
 	return (
 		<>
 			<h1>A random doggo 🐶</h1>
 
+            <div className="mb-3">
+                <Button className="mb-3" onClick={() => setUrl("https://dog.ceo/api/breeds/image/random")}>
+                    Random Doggo
+                </Button>
+                <Button className="mb-3" onClick={() => setUrl("https://dog.ceo/api/breeds/shiba/images/random")}>
+                    Random Shiba doggo
+                </Button>
+            </div>
 			{isLoading && <p>Fetching doggos 🐶...</p>}
 
 			{data && data.status === "success" && (
